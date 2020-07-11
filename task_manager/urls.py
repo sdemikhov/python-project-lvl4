9b4b0f97f5813d477d6e61a495d7e31c18ce89a8
python <python@debian>
2020-07-11 18:49:28 +0400

    update urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
 """
 from django.urls import path
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
 
 from task_manager import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-]    
-#] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
